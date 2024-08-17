8e454d8b8870f39742d05afab3b1ef001a4647e7
MCMoonlight <ruiruixi@hotmail.com>
2024-08-17 23:56:48 +0800

    1211

--- main.py
+++ main.py
@@ -1,80 +1,106 @@
 # coding:utf-8
+
+# create Application
+
 import sys
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QAction, QHBoxLayout, QLabel
-from qfluentwidgets import RoundMenu, setTheme, Theme, Action, MenuAnimationType, MenuItemDelegate, CheckableMenu, MenuIndicatorType
+
+from PyQt5.QtCore import Qt, QUrl, QSharedMemory
+from PyQt5.QtGui import QIcon, QDesktopServices
+from PyQt5.QtWidgets import QApplication, QFrame, QHBoxLayout, QMessageBox
+from qfluentwidgets import (NavigationItemPosition, MessageBox, setTheme, Theme, FluentWindow,
+                            NavigationAvatarWidget, qrouter, SubtitleLabel, setFont, InfoBadge,
+                            InfoBadgePosition, FluentBackgroundTheme)
 from qfluentwidgets import FluentIcon as FIF
 
 
-class Demo(QWidget):
+class Widget(QFrame):
+
+    def __init__(self, text: str, parent=None):
+        super().__init__(parent=parent)
+        self.label = SubtitleLabel(text, self)
+        self.hBoxLayout = QHBoxLayout(self)
+
+        setFont(self.label, 24)
+        self.label.setAlignment(Qt.AlignCenter)
+        self.hBoxLayout.addWidget(self.label, 1, Qt.AlignCenter)
+        self.setObjectName(text.replace(' ', '-'))
+
+
+class Window(FluentWindow):
 
     def __init__(self):
         super().__init__()
-        self.setLayout(QHBoxLayout())
-        self.label = QLabel('Right-click your mouse', self)
-        self.label.setAlignment(Qt.AlignCenter)
-        self.layout().addWidget(self.label)
-        self.resize(400, 400)
 
-        self.setStyleSheet('Demo{background: white} QLabel{font-size: 20px}')
+        # create sub interface
+        self.homeInterface = Widget('home Interface', self)
+        self.settingInterface = Widget('Setting Interface', self)
 
-        # setTheme(Theme.DARK)
+        self.initNavigation()
+        self.initWindow()
 
-    def contextMenuEvent(self, e):
-        menu = RoundMenu(parent=self)
-        # menu = CheckableMenu(parent=self, indicatorType=MenuIndicatorType.RADIO)
+    def initNavigation(self):
+        self.addSubInterface(self.homeInterface, FIF.HOME, 'Home')
+        self.navigationInterface.addSeparator()
 
-        # NOTE: hide the shortcut key
-        # menu.view.setItemDelegate(MenuItemDelegate())
-
-        # add actions
-        menu.addAction(Action(FIF.COPY, 'Copy'))
-        menu.addAction(Action(FIF.CUT, 'Cut'))
-        menu.actions()[0].setCheckable(True)
-        menu.actions()[0].setChecked(True)
-
-        # add sub menu
-        submenu = RoundMenu("Add to", self)
-        submenu.setIcon(FIF.ADD)
-        submenu.addActions([
-            Action(FIF.CHAT, 'wechat'),
-            Action(FIF.MUSIC, 'Music'),
-        ])
-        menu.addMenu(submenu)
-
-        # add actions
-        menu.addActions([
-            Action(FIF.PASTE, 'Paste'),
-            Action(FIF.CANCEL, 'Undo')
-        ])
-
-        # add separator
-        menu.addSeparator()
-        menu.addAction(QAction(f'Select all', shortcut='Ctrl+A'))
-
-        # insert actions
-        menu.insertAction(
-            menu.actions()[-1], Action(FIF.SETTING, 'Settings', shortcut='Ctrl+S'))
-        menu.insertActions(
-            menu.actions()[-1],
-            [Action(FIF.HELP, 'Help', shortcut='Ctrl+H'),
-             Action(FIF.FEEDBACK, 'Feedback', shortcut='Ctrl+F')]
+        # add custom widget to bottom
+        self.navigationInterface.addWidget(
+            routeKey='avatar',
+            widget=NavigationAvatarWidget('zhiyiYo', 'resource/shoko.png'),
+            onClick=self.showMessageBox,
+            position=NavigationItemPosition.BOTTOM,
         )
-        menu.actions()[-2].setCheckable(True)
-        menu.actions()[-2].setChecked(True)
 
-        # show menu
-        menu.exec(e.globalPos(), aniType=MenuAnimationType.DROP_DOWN)
+        self.addSubInterface(self.settingInterface, FIF.SETTING, 'Settings', NavigationItemPosition.BOTTOM)
+
+        # add badge to navigation item
+
+        # NOTE: enable acrylic effect
+        # self.navigationInterface.setAcrylicEnabled(True)
+
+    def initWindow(self):
+        self.resize(900, 700)
+        self.setWindowIcon(QIcon(':/qfluentwidgets/images/logo.png'))
+        self.setWindowTitle('PyQt-Fluent-Widgets')
+
+        desktop = QApplication.desktop().availableGeometry()
+        w, h = desktop.width(), desktop.height()
+        self.move(w//2 - self.width()//2, h//2 - self.height()//2)
+
+        # set the minimum window width that allows the navigation panel to be expanded
+        # self.navigationInterface.setMinimumExpandWidth(900)
+        # self.navigationInterface.expand(useAni=False)
+
+    def showMessageBox(self):
+        w = MessageBox(
+            '支持作者🥰',
+            '个人开发不易，如果这个项目帮助到了您，可以考虑请作者喝一瓶快乐水🥤。您的支持就是作者开发和维护项目的动力🚀',
+            self
+        )
+        w.yesButton.setText('来啦老弟')
+        w.cancelButton.setText('下次一定')
+
+        if w.exec():
+            QDesktopServices.openUrl(QUrl("https://space.bilibili.com/1582724340"))
 
 
 if __name__ == '__main__':
-    # enable dpi scale
-    QApplication.setHighDpiScaleFactorRoundingPolicy(
-        Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+    QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
     QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
     QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
 
+    # setTheme(Theme.DARK)
+
     app = QApplication(sys.argv)
-    w = Demo()
+    w = Window()
     w.show()
     app.exec_()
+    share = QSharedMemory()
+    share.setKey("AutoSprint for Minecraft")
+    if share.attach():
+        msg_box = QMessageBox()
+        msg_box.setWindowTitle("提示")
+        msg_box.setText("软件已在运行!")
+        msg_box.setIcon(QMessageBox.Information)
+        msg_box.addButton("确定", QMessageBox.YesRole)
+        msg_box.exec()
+        sys.exit(-1)
