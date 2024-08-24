0da48d9f605c2e83ff2aa4ad39a8007e7f540e0f
onea_forchina <108773795+oneachina@users.noreply.github.com>
2024-08-24 14:16:35 +0800

    Update AutoSprint.py

--- apps/Module/autosprint/AutoSprint.py
+++ apps/Module/autosprint/AutoSprint.py
@@ -1,13 +1,35 @@
 import keyboard
+import threading
+import time
 
 
 class AutoSprint:
-    def enable(self):
-        print("[info] AutoSprint Enable")
-        keyboard.wait('w')
-        keyboard.press('ctrl')
-        keyboard.release('ctrl')
+    def __init__(self):
+        self.enable = True
+        self.running = False
+        self.thread = threading.Thread(target=self.run)
+        self.thread.start()
 
-    def disabled(self):
-       print("[info] AutoSprint Disabled")
+    def run(self):
+        while True:
+            if self.enable and not self.running:
+                self.running = True
+                print("[info] AutoSprint Enabled")
+                while self.enable:
+                    if keyboard.is_pressed('w'):
+                        keyboard.press('ctrl')
+                        time.sleep(0.1)  # 适当的时间间隔
+                        keyboard.release('ctrl')
+                    time.sleep(0.01)  # 防止 CPU 占用过高
+                self.running = False
+                print("[info] AutoSprint Disabled")
 
+    def toggle(self):
+        self.enable = not self.enable
+
+if __name__ == "__main__":
+    sprint = AutoSprint()
+    while True:
+        if keyboard.is_pressed('I'):
+            sprint.toggle()
+            time.sleep(0.5)  # 防止多次触发
